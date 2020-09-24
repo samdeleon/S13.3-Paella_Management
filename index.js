f40915f003fc625f8816db404f29f98972e0a9f0
JLP603 <60498189+JLP603@users.noreply.github.com>
2020-09-24 22:16:26 +0800

    commented out certain parts to fix login authentication

--- index.js
+++ index.js
@@ -230,13 +230,13 @@ app.post('/newUser', function (req, res) {
 });
 
 app.post('/Login',function (req,res){
-  
+ /* 
   var user = new userModel({
     username:     req.body.username,
     password:     req.body.password,
 
 });
-
+*/
   var result;
   user.findOne({
     username: req.body.username, password: req.body.password
