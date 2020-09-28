497cdf436c89d75c6ee0315484bc5b1ee688aaea
samdeleon <samdeleon@users.noreply.github.com>
2020-09-28 17:39:43 +0800

    removed the extra from extra remarks + linking the name in order info

--- public/script/OrderInformationScript.js
+++ public/script/OrderInformationScript.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+
+    $("#orderinfo-p_name").click(function () {
+        var name =   document.getElementById("orderinfo-span_name").value
+
+        window.location.href = "client-information-" + name
+    });
+    
     $(".info-nextstatusbtn").click(function () {
         var ordernum    =   document.getElementById("ordernumber").value;
         var currstatus  =   document.getElementById("info-obutton").value;
