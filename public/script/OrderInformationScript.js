963f5f7e60634d58805bb6655ab5cf0bab438971
samdeleon <samdeleon@users.noreply.github.com>
2020-09-29 16:56:08 +0800

    made a new modal for assigning pan, will work on connecting it to db

--- public/script/OrderInformationScript.js
+++ public/script/OrderInformationScript.js
@@ -11,23 +11,42 @@ $(document).ready(function () {
         e.preventDefault();
         var ordernum    =   $( "#span_ordernum" ).text();
         var currstatus  =   $( "#span_orderstatus" ).text();
+        var paellasize  =   $( "#info-paellasize" ).text();
         var nextstatus  =   "";
-        var data_success;
+        var next = false; // boolean if they can go to the next status
 
         if(currstatus == "Buying Ingredients") {
             nextstatus = "Complete Ingredients";
+            next = true;
         }
 
         else if(currstatus == "Complete Ingredients") {
             nextstatus = "Cooking";
+            $('#orderstatusModal').modal('toggle');
+
+            // displaying the options depending on the paella size
+            if(paellasize == "14 inches") {
+                $('.choices-14in').show();
+            }
+            else if(paellasize == "16 inches") {
+                $('.choices-16in').show();
+            }
+            else if(paellasize == "20 inches") {
+                $('.choices-20in').show();
+            }
+
+            $('#assignpanModal').modal('show');
+            next = false;   // ill just copy the nextstatus post in the second modal
         }
 
         else if(currstatus == "Cooking") {
             nextstatus = "Delivered"
+            next = true;
         }
 
         else if(currstatus == "Delivered") {
             nextstatus = "Completed"
+            next = true;
         }
 
         var information = {
@@ -35,19 +54,26 @@ $(document).ready(function () {
             ordernum: ordernum
         }
 
-        $.post("nextStatus", information, function(data, status) {
-            if (data.success){
-                $('#orderstatusModal').modal('toggle');
-                window.location.reload();
-                console.log("it working");
-            }
-            else {
-                console.log("it aint working");
-            }
-
-        });
+        if(next) {
+            $.post("nextStatus", information, function(data, status) {
+                if (data.success){
+                    $('#orderstatusModal').modal('toggle');
+                    window.location.reload();
+                    console.log("it working");
+                }
+                else {
+                    console.log("it aint working");
+                }
+    
+            });
+        }
+        
 
         return false;
+    });
+
+    $("#info-assignpanbtn").click(function (e) {
+
     });
 
 });
