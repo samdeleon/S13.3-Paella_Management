fdcc3963d24b85579e104ecb2d732ce16ab5bca5
Shaun Pua <svshaunpuax1@gmail.com>
2020-10-02 01:32:11 +0800

    fixed all display problems for ingredients inventory and plus minus modal

--- public/script/IngredientsInventoryScript.js
+++ public/script/IngredientsInventoryScript.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
 
-
-
+    /*
     $('.count').prop('disabled', true);
-       $(document).on('click','.plus',function(){
+        $(document).on('click','.plus',function(){
         $('.count').val(parseInt($('.count').val()) + 1 );
     });
     $(document).on('click','.minus',function(){
@@ -12,4 +11,259 @@ $(document).ready(function(){
                 $('.count').val(1);
             }
         });
+    */
+    $('#count1').prop('disabled', true);
+       $(document).on('click','#plus1',function(){
+        $('#count1').val(parseInt($('#count1').val()) + 1 );
+    });
+    $(document).on('click','#minus1',function(){
+        $('#count1').val(parseInt($('#count1').val()) - 1 );
+            if ($('#count1').val() == 0) {
+                $('#count1').val(1);
+            }
+        });
+
+    $('#count2').prop('disabled', true);
+        $(document).on('click','#plus2',function(){
+         $('#count2').val(parseInt($('#count2').val()) + 1 );
+     });
+     $(document).on('click','#minus2',function(){
+         $('#count2').val(parseInt($('#count2').val()) - 1 );
+             if ($('#count2').val() == 0) {
+                 $('#count2').val(1);
+             }
+         });
+
+    $('#count3').prop('disabled', true);
+         $(document).on('click','#plus3',function(){
+          $('#count3').val(parseInt($('#count3').val()) + 1 );
+      });
+      $(document).on('click','#minus3',function(){
+          $('#count3').val(parseInt($('#count3').val()) - 1 );
+              if ($('#count3').val() == 0) {
+                  $('#count3').val(1);
+              }
+          });
+    
+     $('#count4').prop('disabled', true);
+          $(document).on('click','#plus4',function(){
+           $('#count4').val(parseInt($('#count4').val()) + 1 );
+       });
+       $(document).on('click','#minus4',function(){
+           $('#count4').val(parseInt($('#count4').val()) - 1 );
+               if ($('#count4').val() == 0) {
+                   $('#count4').val(1);
+               }
+           });
+
+    $('#count5').prop('disabled', true);
+           $(document).on('click','#plus5',function(){
+            $('#count5').val(parseInt($('#count5').val()) + 1 );
+        });
+        $(document).on('click','#minus5',function(){
+            $('#count5').val(parseInt($('#count5').val()) - 1 );
+                if ($('#count5').val() == 0) {
+                    $('#count5').val(1);
+                }
+            });
+
+    $('#count6').prop('disabled', true);
+            $(document).on('click','#plus6',function(){
+             $('#count6').val(parseInt($('#count6').val()) + 1 );
+         });
+         $(document).on('click','#minus6',function(){
+             $('#count6').val(parseInt($('#count6').val()) - 1 );
+                 if ($('#count6').val() == 0) {
+                     $('#count6').val(1);
+                 }
+             });
+
+    $('#count7').prop('disabled', true);
+             $(document).on('click','#plus7',function(){
+              $('#count7').val(parseInt($('#count7').val()) + 1 );
+          });
+          $(document).on('click','#minus7',function(){
+              $('#count7').val(parseInt($('#count7').val()) - 1 );
+                  if ($('#count7').val() == 0) {
+                      $('#count7').val(1);
+                  }
+              });
+
+    $('#count8').prop('disabled', true);
+              $(document).on('click','#plus8',function(){
+               $('#count8').val(parseInt($('#count8').val()) + 1 );
+           });
+           $(document).on('click','#minus8',function(){
+               $('#count8').val(parseInt($('#count8').val()) - 1 );
+                   if ($('#count8').val() == 0) {
+                       $('#count8').val(1);
+                   }
+               });
+
+    $('#count9').prop('disabled', true);
+               $(document).on('click','#plus9',function(){
+                $('#count9').val(parseInt($('#count9').val()) + 1 );
+            });
+            $(document).on('click','#minus9',function(){
+                $('#count9').val(parseInt($('#count9').val()) - 1 );
+                    if ($('#count9').val() == 0) {
+                        $('#count9').val(1);
+                    }
+                });
+
+    $('#count10').prop('disabled', true);
+               $(document).on('click','#plus10',function(){
+                $('#count10').val(parseInt($('#count10').val()) + 1 );
+            });
+            $(document).on('click','#minus10',function(){
+                $('#count10').val(parseInt($('#count10').val()) - 1 );
+                    if ($('#count10').val() == 0) {
+                        $('#count10').val(1);
+                    }
+                });
+
+     $('#count11').prop('disabled', true);
+                $(document).on('click','#plus11',function(){
+                 $('#count11').val(parseInt($('#count11').val()) + 1 );
+             });
+             $(document).on('click','#minus11',function(){
+                 $('#count11').val(parseInt($('#count11').val()) - 1 );
+                     if ($('#count11').val() == 0) {
+                         $('#count11').val(1);
+                     }
+                 }); 
+
+     $('#count12').prop('disabled', true);
+                 $(document).on('click','#plus12',function(){
+                  $('#count12').val(parseInt($('#count12').val()) + 1 );
+              });
+              $(document).on('click','#minus12',function(){
+                  $('#count12').val(parseInt($('#count12').val()) - 1 );
+                      if ($('#count12').val() == 0) {
+                          $('#count12').val(1);
+                      }
+                  });   
+
+    $('#count13').prop('disabled', true);
+                  $(document).on('click','#plus13',function(){
+                   $('#count13').val(parseInt($('#count13').val()) + 1 );
+               });
+               $(document).on('click','#minus13',function(){
+                   $('#count13').val(parseInt($('#count13').val()) - 1 );
+                       if ($('#count13').val() == 0) {
+                           $('#count13').val(1);
+                       }
+                   });   
+    
+    $('#count14').prop('disabled', true);
+                  $(document).on('click','#plus14',function(){
+                   $('#count14').val(parseInt($('#count14').val()) + 1 );
+               });
+               $(document).on('click','#minus14',function(){
+                   $('#count14').val(parseInt($('#count14').val()) - 1 );
+                       if ($('#count14').val() == 0) {
+                           $('#count14').val(1);
+                       }
+                   }); 
+
+    $('#count15').prop('disabled', true);
+                   $(document).on('click','#plus15',function(){
+                    $('#count15').val(parseInt($('#count15').val()) + 1 );
+                });
+                $(document).on('click','#minus15',function(){
+                    $('#count15').val(parseInt($('#count15').val()) - 1 );
+                        if ($('#count15').val() == 0) {
+                            $('#count15').val(1);
+                        }
+                    }); 
+
+    $('#count16').prop('disabled', true);
+                    $(document).on('click','#plus16',function(){
+                     $('#count16').val(parseInt($('#count16').val()) + 1 );
+                 });
+                 $(document).on('click','#minus16',function(){
+                     $('#count16').val(parseInt($('#count16').val()) - 1 );
+                         if ($('#count16').val() == 0) {
+                             $('#count16').val(1);
+                         }
+                     }); 
+
+    $('#count17').prop('disabled', true);
+                     $(document).on('click','#plus17',function(){
+                      $('#count17').val(parseInt($('#count17').val()) + 1 );
+                  });
+                  $(document).on('click','#minus17',function(){
+                      $('#count17').val(parseInt($('#count17').val()) - 1 );
+                          if ($('#count17').val() == 0) {
+                              $('#count17').val(1);
+                          }
+                      }); 
+
+    $('#count18').prop('disabled', true);
+                      $(document).on('click','#plus18',function(){
+                       $('#count18').val(parseInt($('#count18').val()) + 1 );
+                   });
+                   $(document).on('click','#minus18',function(){
+                       $('#count18').val(parseInt($('#count18').val()) - 1 );
+                           if ($('#count18').val() == 0) {
+                               $('#count18').val(1);
+                           }
+                       }); 
+
+    $('#count19').prop('disabled', true);
+                       $(document).on('click','#plus19',function(){
+                        $('#count19').val(parseInt($('#count19').val()) + 1 );
+                    });
+                    $(document).on('click','#minus19',function(){
+                        $('#count19').val(parseInt($('#count19').val()) - 1 );
+                            if ($('#count19').val() == 0) {
+                                $('#count19').val(1);
+                            }
+                        }); 
+
+    $('#count20').prop('disabled', true);
+                        $(document).on('click','#plus20',function(){
+                         $('#count20').val(parseInt($('#count20').val()) + 1 );
+                     });
+                     $(document).on('click','#minus20',function(){
+                         $('#count20').val(parseInt($('#count20').val()) - 1 );
+                             if ($('#count20').val() == 0) {
+                                 $('#count20').val(1);
+                             }
+                         }); 
+
+    $('#count21').prop('disabled', true);
+                         $(document).on('click','#plus21',function(){
+                          $('#count21').val(parseInt($('#count21').val()) + 1 );
+                      });
+                      $(document).on('click','#minus21',function(){
+                          $('#count21').val(parseInt($('#count21').val()) - 1 );
+                              if ($('#count21').val() == 0) {
+                                  $('#count21').val(1);
+                              }
+                          }); 
+
+    $('#count22').prop('disabled', true);
+                          $(document).on('click','#plus22',function(){
+                           $('#count22').val(parseInt($('#count22').val()) + 1 );
+                       });
+                       $(document).on('click','#minus22',function(){
+                           $('#count22').val(parseInt($('#count22').val()) - 1 );
+                               if ($('#count22').val() == 0) {
+                                   $('#count22').val(1);
+                               }
+                           }); 
+
+     $('#count23').prop('disabled', true);
+                           $(document).on('click','#plus23',function(){
+                            $('#count23').val(parseInt($('#count23').val()) + 1 );
+                        });
+                        $(document).on('click','#minus23',function(){
+                            $('#count23').val(parseInt($('#count23').val()) - 1 );
+                                if ($('#count23').val() == 0) {
+                                    $('#count23').val(1);
+                                }
+                            }); 
+                    
+                   
  });
